0cfb2db7a74aafd31723d1896d17012c88153e36
HongTr <truongminhhong0306@gmail.com>
2023-02-23 18:02:01 +0700

    build command successfully

--- actions/build_actions.py
+++ actions/build_actions.py
@@ -4,6 +4,9 @@ from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
 
+listEntity = ['location', 'qos_constraint', "traffic", 'qos_value', 'qos_unit',
+ 'operation', 'service', 'middlebox', 'qos_metric', 'hour', 'group', 'protocol']
+
 class ActionBuild(Action):
     def name(self) -> Text:
         return "action_build"
@@ -12,10 +15,11 @@ class ActionBuild(Action):
              tracker: Tracker,
              domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         dispatcher.utter_message("Your command is: ")
-        location = tracker.get_latest_entity_values("location")
-        if location:
-            dispatcher.utter_message("Location: {location}")
-        operation = tracker.get_latest_entity_values("operation")
-        if location:
-            dispatcher.utter_message("Operation: {location}")
+        for entity in listEntity:
+            names = list(tracker.get_latest_entity_values(entity_type=str(entity)))
+            if len(names) > 0:
+                dispatcher.utter_message(str(entity) + ": ")
+                for name in names:
+                    if name:
+                        dispatcher.utter_message(f"- {name}")
         return []
